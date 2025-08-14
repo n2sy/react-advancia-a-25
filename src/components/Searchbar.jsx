05c81679fa656d311c1bd9891ddfb715451d8ad6
n2sy <nidhal.jelassi@fsegt.utm.tn>
2025-08-14 09:07:42 +0100

    add searchbar

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useRef } from "react";
 
 function Searchbar(props) {
-  return <div>{props.children} </div>;
+  let year1 = useRef();
+  let year2 = useRef();
+  function searchHandler() {}
+  return (
+    <table className="table table-striped">
+      <tbody>
+        <tr>
+          <td>Start Year</td>
+          <td>
+            <input type="number" className="form-control" ref={year1}></input>
+          </td>
+          <td>End Year</td>
+          <td>
+            <input type="number" className="form-control" ref={year2}></input>
+          </td>
+          <td>
+            <button onClick={searchHandler} className="btn">
+              Search
+            </button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  );
 }
 
 export default Searchbar;
